28a64281ae858c931e5781b85d703fb09800a252
artemmel401 <artemmel40@mail.ru>
2023-11-30 02:44:33 +0500

    add review

--- src/mocks/films.ts
+++ src/mocks/films.ts
@@ -5,140 +5,140 @@ export const CardsFilm: Array<FilmCardType> = [
     id: '1',
     name: 'Fantastic Beasts: The Crimes of Grindelwald',
     genre: 'Thriller',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg'
   },
   {
     id: '2',
     name: 'Bohemian Rhapsody',
     genre: 'Drama',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/bohemian-rhapsody.jpg'
   },
   {
     id: '3',
     name: 'Macbeth',
     genre: 'Sci-Fi',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/macbeth.jpg'
   },
   {
     id: '4',
     name: 'Aviator',
     genre: 'Crime',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/aviator.jpg'
   },
   {
     id: '5',
     name: 'We need to talk about Kevin',
     genre: 'Comedy',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/we-need-to-talk-about-kevin.jpg'
   },
   {
     id: '20',
     name: 'What We Do in the Shadows',
     genre: 'Romance',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/what-we-do-in-the-shadows.jpg'
   },
   {
     id: '6',
     name: 'Revenant',
     genre: 'Romance',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/revenant.jpg'
   },
   {
     id: '7',
     name: 'Johnny English',
     genre: 'Crime',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/johnny-english.jpg'
   },
   {
     id: '8',
     name: 'Shutter Island',
     genre: 'Drama',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/shutter-island.jpg'
   },
   {
     id: '9',
     name: 'Pulp Fiction',
     genre: 'Kids & Family',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/pulp-fiction.jpg'
   },
   {
     id: '10',
     name: 'No Country for Old Men',
     genre: 'Drama',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/no-country-for-old-men.jpg'
   },
   {
     id: '19',
     name: 'Snatch',
     genre: 'Horror',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/snatch.jpg'
   },
   {
     id: '11',
     name: 'Moonrise Kingdom',
     genre: 'Comedy',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/moonrise-kingdom.jpg'
   },
   {
     id: '12',
     name: 'Seven Years in Tibet',
     genre: 'Thriller',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/seven-years-in-tibet.jpg'
   },
   {
     id: '13',
     name: 'Midnight Special',
     genre: 'Crime',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/midnight-special.jpg'
   },
   {
     id: '14',
     name: 'War of the Worlds',
     genre: 'Kids & Family',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/war-of-the-worlds.jpg'
   },
   {
     id: '15',
     name: 'Dardjeeling Limited',
     genre: 'Comedy',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/dardjeeling-limited.jpg'
   },
   {
     id: '16',
     name: 'Orlando',
     genre: 'Crime',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/orlando.jpg'
   },
   {
     id: '17',
     name: 'Mindhunter',
     genre: 'Thriller',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/mindhunter.jpg'
   },
   {
     id: '18',
     name: 'Midnight Special',
     genre: 'Kids & Family',
-    released: 0,
+    previewVideoLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
     previewImage: 'img/midnight-special.jpg'
   },
 ];
