1f0ab0262fb5d28bbe25ecf1e91bbb5eb564cd46
artemmel401 <artemmel40@mail.ru>
2023-12-05 18:15:03 +0500

    add myList

--- src/mocks/seeReviewFilm.ts
+++ src/mocks/seeReviewFilm.ts
@@ -1,6 +1,6 @@
-import { SeeReviewFilmType } from '../types/films';
+import { SeeReviewFilm } from '../types/films';
 
-export const SeeReviewFilm: Array<SeeReviewFilmType> = [
+export const SeeRevFilm: Array<SeeReviewFilm> = [
   {
     id: 'ac8adb38-0cfa-4585-a767-d3afcc557a05',
     date: '2023-05-25T12:00:00.000Z',
